3b72c5d9640ea383e68fe16ed68410626d25f3ab
Sosa09 <soufiane.arrazouki@hotmail.com>
2024-11-28 20:42:47 +0100

    Add project files.

---
+++ PowerBsRise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PowerBsRise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
