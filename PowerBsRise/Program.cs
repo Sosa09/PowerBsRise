3d6ecbc240804243ba720dd001a59838c02d4c8d
Sosa09 <soufiane.arrazouki@hotmail.com>
2025-02-14 19:19:07 +0100

    renamed opening hour to operating hours to match the initial class name
    created class operatinghourservice that will handle the conversion from human language to cms language as well as getting data of the existing database

--- PowerBsRise/Program.cs
+++ PowerBsRise/Program.cs
@@ -111,11 +111,11 @@ namespace PowerBsRise
             {
                 switch (menuOptionIndex)
                 {
-                    case Constants.OPENING_HOURS_FETCH:
+                    case Constants.OPERATING_HOUR_FETCH_OPTION:
                         break;
-                    case Constants.OPENING_HOURS_UPDATE:
+                    case Constants.OPERATING_HOUR_UPDATE_OPTION:
                         break;
-                    case Constants.OPENING_HOURS_CONVERT:
+                    case Constants.OPERATING_HOUR_CONVERT_OPTION:
                         break;
                     default:
                         break;
