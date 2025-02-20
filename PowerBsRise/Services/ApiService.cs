6a3e339dcb6faf936be5f1166a1bf5240fd31947
Sosa09 <soufiane.arrazouki@hotmail.com>
2025-02-20 18:34:01 +0100

    making sure only one instance of the api service class is being created using the singleton pattern. also making sure one thread can access the block at a time where the instance is created.

--- PowerBsRise/Services/ApiService.cs
+++ PowerBsRise/Services/ApiService.cs
@@ -9,5 +9,22 @@ namespace PowerBsRise.Services
     //class will handle the http request to and from broadsign cms
     public class ApiService
     {
+        private ApiService _instance;
+        private object _instanceLock = new object();
+        public ApiService Instance 
+        { 
+            get 
+            { 
+                lock (_instanceLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new ApiService();
+                    }
+                }
+                return _instance; 
+            } 
+        }
+        private ApiService() { }
     }
 }
