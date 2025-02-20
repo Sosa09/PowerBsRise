e09da5305589b75772bb8a4ac8efd95dbd9914f7
Sosa09 <soufiane.arrazouki@hotmail.com>
2025-02-20 18:28:31 +0100

    created base classes for api data handler which will handle the data to and from broadsign and also api service which will handle the requests get post put

---
+++ PowerBsRise/Services/ApiService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerBsRise.Services
+{
+    //class will handle the http request to and from broadsign cms
+    public class ApiService
+    {
+    }
+}
